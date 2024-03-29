d0190f5af2fc8e4883b7981db1ad3e7bedecd493
JuanCarlos-A <juancar2805@gmail.com>
2024-03-29 11:26:26 -0500

    Remove Duplicate

--- leetcode_solution/remove_duplicate.py
+++ leetcode_solution/remove_duplicate.py
@@ -1,3 +1,13 @@
 class Solution:
-    def removeDuplicates(self, nums : list[int]) -> int:
-        pass
+    @staticmethod
+    def removeDuplicates(nums : list[int]) -> int:
+        current = 0
+        last = None
+        for i in nums:
+            if i == last: continue  # noqa: E701
+            nums[current] = i
+            last = i
+            current += 1
+        return current
+
+print(Solution.removeDuplicates([-1,0,0,0,0,3,3]))
